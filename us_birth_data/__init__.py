ba704adacbe38708275f27ff9e8d6399864d6b28
Christopher Boyd <mikuana@gmail.com>
2020-10-18 20:09:22 -0700

    Setting up for end-to-end data generation

--- us_birth_data/__init__.py
+++ us_birth_data/__init__.py
@@ -2,7 +2,8 @@
 Blah blah blah module
 """
 
-from us_birth_data.data import get_data, Year, Month, DayOfWeek, State, Births
+from us_birth_data.data import get_data
+from us_birth_data.fields import Year, Month, DayOfWeek, State, Births
 
 __version__ = '0.0.0'  # this doesnt work yet
 
