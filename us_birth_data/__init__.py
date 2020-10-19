fd0437a168557e4e80027df8058a5ec03d29238f
Christopher Boyd <mikuana@gmail.com>
2020-10-18 19:59:12 -0700

    Downgrading version

--- us_birth_data/__init__.py
+++ us_birth_data/__init__.py
@@ -4,7 +4,7 @@ Blah blah blah module
 
 from us_birth_data.data import get_data, Year, Month, DayOfWeek, State, Births
 
-__version__ = '0.0.1'
+__version__ = '0.0.0'  # this doesnt work yet
 
 __all__ = [
     'get_data', 'Year', 'Month', 'DayOfWeek', 'State', 'Births'
