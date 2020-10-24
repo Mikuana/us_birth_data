58639ef03b8d508cba06cd4dfa9c7c2f0cafa911
Christopher Boyd <mikuana@gmail.com>
2020-10-23 21:50:23 -0700

    Adding Year files tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,13 @@
+import tempfile
+from pathlib import Path
+
 import pytest
+
+
+@pytest.fixture()
+def tmpdir():
+    with tempfile.TemporaryDirectory() as d:
+        yield Path(d)
 
 
 # noinspection SpellCheckingInspection
