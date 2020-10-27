28d7779a0cf36b9ecd6796345d71928b7695a926
Christopher Boyd <mikuana@gmail.com>
2020-10-26 22:03:24 -0700

    Adding child sex field

--- us_birth_data/fields.py
+++ us_birth_data/fields.py
@@ -371,6 +371,43 @@ class DeliveryMethod(OriginalColumn):
             return 'Unknown'
 
 
+class SexOfChild(OriginalColumn):
+    """ Sex of child """
+    handler = Handlers.integer
+    labels = {1: 'Male', 2: 'Female', 9: 'Unknown'}
+    pd_type = 'category'
+    positions = {
+        Y1968: (31, 31),
+        **{
+            x: (35, 35) for x in
+            (Y1969, Y1970, Y1971, Y1972, Y1973, Y1974, Y1975, Y1976, Y1977, Y1978,
+             Y1979, Y1980, Y1981, Y1982, Y1983, Y1984, Y1985, Y1986, Y1987, Y1988)
+        },
+        **{
+            x: (189, 189) for x in
+            (Y1989, Y1990, Y1991, Y1992, Y1993, Y1994, Y1995, Y1996, Y1997, Y1998,
+             Y1999, Y2000, Y2001, Y2002)
+        }
+    }
+
+
+class Sex(SexOfChild):
+    """ Sex of child """
+    handler = Handlers.character
+    labels = {'M': 'Male', 'F': 'Female', 9: 'Unknown'}
+    positions = {
+        **{
+            x: (436, 436) for x in
+            (Y2003, Y2004, Y2005, Y2006, Y2007, Y2008, Y2009, Y2010, Y2011,
+             Y2012, Y2013)
+        },
+        **{
+            x: (475, 475) for x in
+            (Y2014, Y2015, Y2016, Y2017, Y2018, Y2019)
+        },
+    }
+
+
 final_fields = [
-    Year, Month, DayOfWeek, DeliveryMethod, State, Births
+    Year, Month, DayOfWeek, DeliveryMethod, SexOfChild, State, Births
 ]
