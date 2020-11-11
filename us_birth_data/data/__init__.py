15becb1d60b1a547492cb37690a3a0f6e466cd64
Christopher Boyd <mikuana@gmail.com>
2020-11-11 13:46:13 -0800

    Correct JSON error

--- us_birth_data/data/__init__.py
+++ us_birth_data/data/__init__.py
@@ -5,4 +5,4 @@ folder = Path(__file__).parent
 full_data = Path(folder, 'us_birth_data.parquet')
 gzip_data = Path(full_data.as_posix() + '.gz')
 
-hashes = json.loads(Path(folder, 'hashes.json'))
+hashes = json.loads(Path(folder, 'hashes.json').read_text())
